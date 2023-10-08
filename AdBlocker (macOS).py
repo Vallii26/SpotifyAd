9b70e98a06ffa533e51590ea4aaf6660cbbbbce7
Vallii26 <118137290+Vallii26@users.noreply.github.com>
2023-10-07 20:43:00 -0400

    Update AdBlocker (macOS).py

--- AdBlocker (macOS).py
+++ AdBlocker (macOS).py
@@ -1,25 +1,21 @@
 import subprocess
 import time
 
-# Function to open Spotify and play the playlist
 def open_spotify_and_play(playlist_uri):
     app_name = "Spotify"
     try:
         subprocess.call(["open", "-a", app_name])
     except FileNotFoundError:
         print(f"The application '{app_name}' was not found.")
-    
-    # Wait for a few seconds to give Spotify time to open
+
     time.sleep(3)
-    
-    # Play the specified playlist using AppleScript
+
     try:
         applescript = f'tell application "{app_name}" to play track "{playlist_uri}"'
         subprocess.call(["osascript", "-e", applescript])
     except subprocess.CalledProcessError:
         print(f"Failed to play playlist in '{app_name}'.")
 
-# Function to close Spotify
 def close_spotify():
     app_name = "Spotify"
     try:
@@ -27,7 +23,6 @@ def close_spotify():
     except subprocess.CalledProcessError:
         print(f"Failed to close '{app_name}'.")
 
-# Function to check the current track's duration in seconds
 def get_track_duration():
     app_name = "Spotify"
     try:
@@ -40,28 +35,23 @@ def is_spotify_running():
     return subprocess.call(["pgrep", "-x", "Spotify"]) == 0
 
 
-# Replace with the actual Spotify playlist URI you want to play
 playlist_uri = "spotify:playlist:YOUR_PLAYLIST_URI"
 
 
-# Continuously monitor Spotify's state
 while True:
 
     if not is_spotify_running():
         open_spotify_and_play(playlist_uri)
 
     if is_spotify_running:
-        # Get the current track's duration in seconds
         current_duration = get_track_duration()
         print(f'Track Duration: {current_duration // 1000}')
-        
-        # If the current duration is 30 seconds or less, close Spotify
+
         if current_duration // 1000 == 30:
             close_spotify()
             time.sleep(2)
             open_spotify_and_play(playlist_uri)
-    
-    # Sleep for a short interval (e.g., 5 seconds) before checking again
+
     time.sleep(5)
 
 
