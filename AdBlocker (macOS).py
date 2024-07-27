907ba0bb19d1f5661d79022df29406d1d05d2817
Vallii26 <118137290+Vallii26@users.noreply.github.com>
2024-07-27 14:14:17 +0200

    Update AdBlocker (macOS).py

--- AdBlocker (macOS).py
+++ AdBlocker (macOS).py
@@ -4,7 +4,7 @@ import time
 def open_spotify_and_play(playlist_uri):
     app_name = "Spotify"
     try:
-        subprocess.call(["open", "-a", app_name])
+        subprocess.call(["open", "-g", "-a", app_name])
     except FileNotFoundError:
         print(f"The application '{app_name}' was not found.")
 
@@ -30,28 +30,30 @@ def get_track_duration():
         return int(duration.decode("utf-8").strip())
     except subprocess.CalledProcessError:
         return 0
-    
+
 def is_spotify_running():
     return subprocess.call(["pgrep", "-x", "Spotify"]) == 0
 
+playlist_uri = "YOUR PLAYLIST URI HERE"
 
-playlist_uri = "spotify:playlist:YOUR_PLAYLIST_URI"
-
-
+program_initiated_close = False
 while True:
-
     if not is_spotify_running():
-        open_spotify_and_play(playlist_uri)
+        if program_initiated_close:
+            program_initiated_close = False
+        else:
+            break
 
-    if is_spotify_running:
+    if is_spotify_running():
         current_duration = get_track_duration()
         print(f'Track Duration: {current_duration // 1000}')
 
-        if current_duration // 1000 == 30 or current_duration // 1000 == 15:
+        if 15 <= current_duration // 1000 <= 30:
             close_spotify()
+            program_initiated_close = True
             time.sleep(2)
             open_spotify_and_play(playlist_uri)
-
+            
     time.sleep(5)
 
 
